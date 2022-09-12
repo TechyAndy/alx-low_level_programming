5df3f063470b77273ba9b68983269071570d87bb
TechyAndy <Templepounce@gmail.com>
2022-09-12 10:05:15 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main- for puts 
+ *return: return (0) 
+ */
+int main(0)
+{
+	puts("\Programming is like building a multilingual puzzle");
+	return (0);
+}
+
