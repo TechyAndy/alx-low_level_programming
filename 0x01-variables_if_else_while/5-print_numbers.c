9346ea5328a3086cf75d32f894dbe88bf697802b
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-12 20:33:39 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,38 +1,31 @@
 /*
- *
- *  * File: 5-print_numbers.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+ * File: 5-print_numbers.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- *  * main - Prints all single digit numbers of base 10 starting from 0.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	int num;
 
-		int num;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
+	printf("\n");
 
-
-			for (num = 0; num < 10; num++)
-
-						printf("%d", num);
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pr
 
 
 
