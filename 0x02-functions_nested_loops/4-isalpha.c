7fba8f3ee7140d2dc39d9ff2e35e551ccca6f849
TechyAndy <Templepounce@gmail.com>
2022-09-13 19:10:49 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isalpha - check the code for Alx software Engineering trainees.
+ *
+ *   *@c: letter being tested
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+
+
+int _isalpha(int c)
+
+{
+
+		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
