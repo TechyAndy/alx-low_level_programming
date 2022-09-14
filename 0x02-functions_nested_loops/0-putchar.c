669efb81be4b28b51938e6e0af8cd17fe9e8f5cf
TechyAndy <Templepounce@gmail.com>
2022-09-13 17:27:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *
+ *  main - Prints main
+ *
+ *  
+ *
+ *   Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+
+{
+
+      _putchar('_');
+
+      _putchar('p');
+
+      _putchar('u');
+
+      _putchar('t');
+
+      _putchar('c');
+
+      _putchar('h');
+
+      _putchar('a');
+
+      _putchar('r');
+
+										 return (0);
+
+
+}
