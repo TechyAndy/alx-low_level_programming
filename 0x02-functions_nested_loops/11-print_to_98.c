4fbd4f0649adcef209309406d84763cc0ba40f02
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-13 19:42:52 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,39 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * times_table - prints the times table of an integer starting with zero
- *
- * Return: Always 0.
+ * print_to_98 - prints all natural numbers from n to 98
+ *@n: starting point
+ * Return: returns nothing
  */
 
-void times_table(void)
+void print_to_98(int n)
 {
-int x, y, product;
-for (x = 0; x <= 9; x++)
-{
-for (y = 0; y <= 9; y++)
-{
-product = x * y;
-if (y != 0)
-{
-_putchar(',');
-_putchar(' ');
-}
-if (y == 0)
-{
-_putchar('0');
-}
-else if (product >= 10)
-{
-_putchar((product / 10) + '0');
-_putchar((product % 10) + '0');
-}
-else if ((product < 10) && (y != 0))
-{
-_putchar(' ');
-_putchar((product % 10) + '0');
-}
-}
-_putchar('\n');
-}
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
