15b95bd8dea1c1f35bd0809b6a9c5b16e1b93604
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-18 15:44:34 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,31 +1,36 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
- * main - entry point
- *
- * Description: prints FizzBuzz
- *
- * Return: void
- */
+* main - prints the numbers from 1 to 100, followed by a new line
+* but for multiples of three prints Fizz instead of the number
+* and for the multiples of five prints Buzz
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
+int i;
 
-	for (n = 1; n <= 100; n++)
-	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-			printf("FizzBuzz");
-		else if (n % 5 == 0)
-			printf("Buzz");
-		else if (n % 3 == 0)
-			printf("Fizz");
-		else
-			printf("%i", n);
-
-		if (n < 100)
-			printf(" ");
-		else
-			printf("\n");
-	}
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+} else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+} else if (i == 1)
+{
+printf("%d", i);
+} else
+{
+printf(" %d", i);
 }
+}
+printf("\n");
+
+return (0);
+} 
