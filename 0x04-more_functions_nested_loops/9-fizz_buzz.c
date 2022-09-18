2e666524b97694f87c5ea5cf16069a2044fcc64e
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-18 16:23:48 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,14 @@
 * and for the multiples of five prints Buzz
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if (i % 3 == 0  &&  i % 5 != 0)
 {
 printf(" Fizz");
 } else if (i % 5 == 0 && i % 3 != 0)
