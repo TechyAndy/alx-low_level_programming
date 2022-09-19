97d35c55d26839d934012a4f5e2e52dee955797e
TechyAndy <Templepounce@gmail.com>
2022-09-19 13:07:48 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _puts - Entry point
+ *
+ *   * Desc: Entry
+ *
+ *    *@str: pointer
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+void _puts(char *str)
+
+{
+
+		int i;
+
+
+
+			for (i = 0 ; str[i] != '\0' ; i++)
+
+					{
+
+								_putchar(str[i]);
+
+									}
+
+				_putchar('\n');
+
+}
