a8d40e8462a0c39c023db6709e45f4068afa593a
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-19 19:52:49 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,13 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * reset_to_98 - Entry point
- *
- *   * Desc: Entry
- *
- *    *@n: pointer
- *
- *     * Return: Always 0 (Success)
- *
- *      */
-
+ * reset_to_98 - Entry point
+ * Desc: Entry
+ *@n: pointer
+ * Return: Always 0 (Success)
+ */
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
 
