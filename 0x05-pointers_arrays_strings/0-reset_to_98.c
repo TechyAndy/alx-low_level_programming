37afc70e84b4e65c91820a69b360182a317ec5b8
TechyAndy <Templepounce@gmail.com>
2022-09-19 11:51:28 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * reset_to_98 - Entry point
+ *
+ *   * Desc: Entry
+ *
+ *    *@n: pointer
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+
+}
+
