e75c789ba7b92a24f2057ce5b3cf4356fa9c1be0
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-19 21:31:47 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,45 +1,23 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_array - Entry point
- *
- *   * Desc: Entry
- *
- *    *@a: pointer
- *
- *     *@n: var
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ * print_array - Entry point
+ * Desc: Entry
+ *@a: pointer
+ *@n: var
+ * Return: Always 0 (Success)
+ */
 void print_array(int *a, int n)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0 ; i < n ; i++)
-
-					{
-
-								printf("%d", *(a + i));
-
-										if (i < (n - 1))
-
-													{
-
-																	printf(", ");
-
-																			}
-
-											}
-
-				printf("\n");
-
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", *(a + i));
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
