1111a775349a69101332f2622e5358206caa728e
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-19 19:57:09 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,36 +1,22 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * swap_int - Entry point
- *
- *   * Desc: Entry
- *
- *    *@a: pointer
- *
- *     *@b: pointer
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ * swap_int - Entry point
+ * Desc: Entry
+ *@a: pointer
+ *@b: pointer
+ * Return: Always 0 (Success)
+ */
 void swap_int(int *a, int *b)
-
 {
+	int c;
 
-		int c;
-
-
-
-			c = *a;
-
-				*a = *b;
-
-					*b = c;
-
+	c = *a;
+	*a = *b;
+	*b = c;
 }
+
+
 
 
 
