93b2e0793176413eec35e44053248360d5c6c445
TechyAndy <Templepounce@gmail.com>
2022-09-22 17:44:49 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,54 @@
-/**
- * _strcat - concatenates two strings
- * @dest: string to concate with src
- * @src: string to concate to dest
- * Return: pointer to dest
- */
-char *_strcat(char *dest, char *src)
-{
-	char *p = dest;
+#include "main.h"
 
-	while (*dest++)
-		;
-	dest--;
-	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (p);
+/**
+ *
+ *  * _strcat - entry point
+ *
+ *   * @dest: string destination
+ *
+ *    * @src: string source
+ *
+ *     * Description: prints a string, followed by a new line, to stdout.
+ *
+ *      * Return: pointer to the resulting string dest
+ *
+ *       */
+
+
+
+char *_strcat(char *dest, char *src)
+
+{
+
+
+
+	char *ptr;
+
+	int longitud = 0;
+
+
+
+	while (dest[longitud] != '\0')
+
+		longitud++;
+
+
+
+	ptr = dest + longitud;
+
+
+
+	while (*src != '\0')
+
+		*ptr++ = *src++;
+
+
+
+	*ptr = '\0';
+
+
+
+	return (dest);
+
 }
+
