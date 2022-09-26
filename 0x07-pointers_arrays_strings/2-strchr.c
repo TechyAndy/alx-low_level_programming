d840de3b3670c16bbf4a5defa0161d39882d15eb
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-09-26 10:16:50 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,49 +1,25 @@
 #include "main.h"
-
 /**
+ * _strchr - prints from the first occurrence of a char.
+ * @s: source string
+ * @c: tested char
  *
- *  * _strchr - prints from the first occurrence of a char.
- *
- *   * @s: source string
- *
- *    * @c: tested char
- *
- *     *
- *
- *      * Return: new string.
- *
- *       */
-
+ * Return: new string.
+ */
 char *_strchr(char *s, char c)
-
 {
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
 
-		while (*s != '\0')
-
-				{
-
-							if (*s == c)
-
-										{
-
-														return (s);
-
-
-
-																}
-
-									else
-
-													s++;
-
-										}
-
-			if (*s == c)
-
-						return (s);
-
-				else
-
-							return (0);
-
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
 }
