6b5699cf07d43640e2006fda9769641191ec071d
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-10-19 09:43:19 -0700

    Update main.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,13 +1,6 @@
-#ifndef _MAIN_
-
-#define _MAIN_
-
-
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-
-
 #endif /* MAIN_H */
