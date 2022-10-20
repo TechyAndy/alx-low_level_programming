403131254f63dac61f7ad09f2c10f593c9f8f3ea
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-10-20 01:47:01 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,25 +1,13 @@
 #include "1-pi.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code for Holberton School students.
  *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
-		printf("%s\n", __FILE__);
-
-			return (0);
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
