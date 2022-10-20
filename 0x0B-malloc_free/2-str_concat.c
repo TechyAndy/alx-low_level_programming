30434d1ac5c721a1f3b5b29cf9561c07ac438fbd
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-10-20 00:54:27 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,103 +1,41 @@
-#include "main.h"
-
-
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- *  * str_concat - concatenate two strings
- *
- *   * @s1: a pointer to the first string
- *
- *    * @s2: a pointer to the second string
- *
- *     *
- *
- *      * Return: NULL if memory allocation fails,
- *
- *       * otherwise a pointer to a the new string
- *
- *        */
-
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
+ */
 char *str_concat(char *s1, char *s2)
-
 {
+	char *strnew = NULL;
+	unsigned int i;
+	int n1;
+	int n2;
+	int count;
 
-		unsigned int len1 = 0;
-
-			unsigned int len2 = 0;
-
-				char *cat;
-
-
-
-					if (s1)
-
-							{
-
-										while (s1[len1])
-
-														++len1;
-
-											}
-
-						else
-
-								{
-
-											s1 = "";
-
-												}
-
-							if (s2)
-
-									{
-
-												while (s2[len2])
-
-																++len2;
-
-													}
-
-								else
-
-										{
-
-													s2 = "";
-
-														}
-
-
-
-									cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-
-
-
-										if (!cat)
-
-													return (NULL);
-
-											if (s1)
-
-													{
-
-																for (len1 = 0; s1[len1]; ++len1)
-
-																				cat[len1] = s1[len1];
-
-																	}
-
-												if (s2)
-
-														{
-
-																	for (len2 = 0; s2[len2]; ++len2)
-
-																					cat[len1 + len2] = s2[len2];
-
-																		}
-
-													cat[len1 + len2] = '\0';
-
-														return (cat);
-
+	count = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (; s2[count] != '\0'; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+	}
+	return (strnew);
 }
