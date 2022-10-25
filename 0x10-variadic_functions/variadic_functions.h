1d774562098d7d979f597dd0a08b1a95835d164d
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-10-25 01:34:36 -0700

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,45 +1,24 @@
-#ifndef VARIADIC_FUNCTIONS_H
-
-#define VARIADIC_FUNCTIONS_H
-
-
-
+#ifndef VARDIAC_FUNCTIONS
+#define VARDIAC_FUNCTIONS
+#include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <stdarg.h>
-
-
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 /**
- *
- *  *struct ty - structure for functions to print
- *
- *   *@func: char that defines the function
- *
- *    *@p: pointer to the function
- *
- *     */
-
-typedef struct ty
-
+ * struct format - struct for task 3.
+ * @format_in: id of format
+ * @function: pointer to function that print with @format_in
+ **/
+typedef struct format
 {
-
-		char func;
-
-			void (*p)(va_list);
-
-} type;
-
-int _putchar(char c);
-
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
+	char format_in;
+	void (*function)(va_list);
+} format_t;
 
 #endif
