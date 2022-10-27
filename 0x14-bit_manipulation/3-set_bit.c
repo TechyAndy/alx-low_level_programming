99a5067d1321272b88640529f603a60af32c960e
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-10-27 06:56:17 -0700

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,37 +1,19 @@
-#include "main.h"
-
-
+#include "mai.h"
 
 /**
- *
- *  * set_bit - set value to 1 with given index
- *
- *   * @n:pointer to unsigned int
- *
- *    * @index: unsigned int index
- *
- *     * Return: 1 if succes -1 in case of error
- *
- *      */
-
+ * set_bit - set value to 1 with given index
+ * @n:pointer to unsigned int
+ * @index: unsigned int index
+ * Return: 1 if succes -1 in case of error
+ */
 int set_bit(unsigned long int *n, unsigned int index)
-
 {
+	unsigned int x = 1;
 
-		unsigned int x = 1;
+	if (sizeof(n) * 8 < index)
+		return (-1);
 
-
-
-			if (sizeof(n) * 8 < index)
-
-						return (-1);
-
-
-
-			x <<= index;
-
-			*n |= x;
-
-			return (1);
-
+x <<= index;
+*n |= x;
+return (1);
 }
