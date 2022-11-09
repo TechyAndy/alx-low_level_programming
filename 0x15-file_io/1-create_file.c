c240388da2f93609941861c6560b4c7a69f2c738
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-11-09 07:26:25 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,99 +1,50 @@
 #include "main.h"
 
-
-
 /**
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
  *
- *  * _strlen - compute the length of a NULL-terminated string
- *
- *   * @str: the string to measure
- *
- *    *
- *
- *     * Return: the length of str, or -1 if str is NULL
- *
- *      */
-
+ * Return: the length of str, or -1 if str is NULL
+ */
 ssize_t _strlen(const char *str)
-
 {
+	ssize_t len = 0;
 
-		ssize_t len = 0;
+	if (!str)
+		return (-1);
 
+	while (*str++)
+		++len;
 
-
-			if (!str)
-
-						return (-1);
-
-
-
-				while (*str++)
-
-							++len;
-
-
-
-					return (len);
-
+	return (len);
 }
 
-
-
 /**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
  *
- *  * create_file - create a file
- *
- *   * @filename: the name of the file to create
- *
- *    * @text_content: the data to write to filename
- *
- *     *
- *
- *      * Return: Upon success, return 1. Otherwise, return -1.
- *
- *       */
-
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
 int create_file(const char *filename, char *text_content)
-
 {
+	ssize_t b_written = 0;
+	int fd;
 
-		ssize_t b_written = 0;
+	if (!filename)
+		return (-1);
 
-			int fd;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
+	if (fd < 0)
+		return (-1);
 
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-				if (!filename)
+	close(fd);
 
-							return (-1);
-
-
-
-					fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-
-
-						if (fd < 0)
-
-									return (-1);
-
-
-
-							if (text_content)
-
-										b_written = write(fd, text_content, _strlen(text_content));
-
-
-
-								close(fd);
-
-
-
-									if (b_written < 0)
-
-												return (-1);
-
-										return (1);
-
+	if (b_written < 0)
+		return (-1);
+	return (1);
 }
