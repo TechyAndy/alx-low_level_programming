6ac9d9ded9d18faf199cfdde740cd89d44e90e4f
TechyAndy <111127774+TechyAndy@users.noreply.github.com>
2022-12-08 15:26:25 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *string;
 	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korpar, 2015-10-2019";
+	char str2[] = "- Dora Korpar, 2015-10-19";
 
 	string = malloc(strlen(str1) + strlen(str2) + 1);
 	strcpy(string, str1);
